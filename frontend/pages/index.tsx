80924ab7a393d46f7511f0aedab3a6fecc0dbd56
Tobin South <tobin.south@gmail.com>
2023-01-28 15:39:29 -0500

    Fixing home page style

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -16,19 +16,19 @@ export default function Home() {
                 <meta name="description" content="A generic landing page" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className={styles.main}>
-            <div className={`${styles.coolBackground} w-full flex justify-center items-center py-2 strong`}>
-                    <div className="w-full flex justify-center items-center">
-                        <h1 className="text-xl">
-                            zk Tax Auditing
-                        </h1>
-                    </div>
+            <div className={`w-full flex justify-center items-center py-2 strong`}>
+                <div className="w-full flex justify-center items-center">
+                    <h1 className="text-xl">
+                        zk Tax Auditing
+                    </h1>
                 </div>
+            </div>
+            <div className="flex flex-col justify-center items-center">
                 <p className="mb-2">You can go to any of the follow three pages.</p>
-                <div className="flex flex2 flex-row ">
-                    <Link href="/signtaxform" passHref> <Button backgroundColor="black" color="white"> Sign Tax PDF </Button> </Link>
+                <div className="flex space-x-4 flex-row ">
+                    <Link href="/taxservice" passHref> <Button backgroundColor="black" color="white"> Sign Tax PDF </Button> </Link>
 
-                    <Link href="/prove"><button className="py-2 px-4 rounded-md hover:opacity-70 bg-black text-white">Create Redacted Audit & Proof</button></Link>
+                    <Link href="/prove"><Button backgroundColor="black" color="white">Create Redacted Audit & Proof</Button></Link>
                     
                     <Link href="/verify"><Button backgroundColor="black" color="white">Verify Tax Proof</Button></Link>
                 </div>
