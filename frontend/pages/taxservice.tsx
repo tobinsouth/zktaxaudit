360798edd473eb95b85dd7d0bcb1dc215bfa6859
Tobin South <tobin.south@gmail.com>
2023-01-28 22:30:46 -0500

    Signing JSON input with a known ed25519 keypair

--- frontend/pages/taxservice.tsx
+++ frontend/pages/taxservice.tsx
@@ -2,8 +2,71 @@ import Head from "next/head";
 import React, { useEffect, useState } from "react";
 import Link from 'next/link'
 
+// Signing stuff with ed & storing keys in localforage 
+import * as ed from "@noble/ed25519";
+import localforage from "localforage";
+
+import { Textarea } from "../components/textarea";
+import toast, { Toaster } from "react-hot-toast";
+import { JsonViewer } from "@textea/json-viewer";
+
 
 export default function TaxService() {
+    const [isLoading, setIsLoading] = useState<number | undefined>(2);
+    const [hasKeypair, setHasKeypair] = useState<boolean>(false);
+    const [jsonTextInput, setJsonTextInput] = useState<string>('{"fname": "Alex", "lname": "Berke", "SSN":"123456789", "10b": 1000, "24": 3000, "year":"2020", "form":"1040" }');
+    const [jsonOutput, setJsonOutput] = useState({});
+
+    useEffect(() => {
+        // This function checks that the browser session has a valid keypair stored in localforage or else generates one
+        async function checkIsRegistered() {
+            const maybePrivKey = await localforage.getItem("zkattestorPrivKey");
+            const maybePubKey = await localforage.getItem("zkattestorPubKey");
+            if (maybePrivKey && maybePubKey) {
+                setHasKeypair(true);
+                console.log("Keypair found");
+            } else {
+                setIsLoading(0);
+                const privKey = ed.utils.randomPrivateKey();
+                const publicKey = await ed.getPublicKey(privKey);
+                await localforage.setItem("zkattestorPrivKey", privKey);
+                await localforage.setItem("zkattestorPubKey", publicKey);
+                console.log("Generated new keypair");
+                setIsLoading(undefined);
+                setHasKeypair(true);
+            }
+        }
+        checkIsRegistered();
+    }, []);
+
+
+    
+    // A function to sign a JSON object with the private key
+    async function signJson(json: string) {
+
+        // TODO: Use checkJsonSchema to validate the JSON input
+        console.log("Trying to parse JSON", json);
+        const parsedJSON = JSON.parse(json);
+        
+
+        const privKey = await localforage.getItem("zkattestorPrivKey");
+        const publicKey = await localforage.getItem("zkattestorPubKey");
+        if (privKey && publicKey) {
+            // We quickly convert to JSON to a Uint8Array to sign
+            const jsonUint8 = new TextEncoder().encode(json);
+            const signature = await ed.sign(jsonUint8, privKey);
+            const signedJson = {
+                "json": parsedJSON,
+                "servicePubkey": publicKey,
+                "signature": signature
+            }
+            setJsonOutput(signedJson);
+            toast.success("JSON signed");
+        } else {
+            toast.error("No keypair found")
+        }
+    }
+
     return (
         <>
             <Head>
@@ -12,6 +75,28 @@ export default function TaxService() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Link href="/">Home</Link>
+            <div>
+                <h1>Sign Tax PDF</h1>
+                <p>This page will eventually take in a tax pdf and produce a signed JSON. For the time being we will take in manual values and produce a signed JSON output. </p>
+            </div>
+            <div style={{ width: "800px" }} className="flex flex-col justify-center items-center">
+                    <Textarea
+                        placeholder={"Input original JSON here"}
+                        value={jsonTextInput}
+                        onChangeHandler={(newVal: string) => {
+                            setJsonTextInput(newVal);
+                        }}
+                    />
+                    <button onClick={() => signJson(jsonTextInput)}>Sign JSON</button>
+                    {jsonOutput ? (
+                        <>
+                            <div className="py-2"></div>
+                            <JsonViewer value={jsonOutput} />
+                        </>
+                    ) : null}
+
+            </div>
+
         </>
     );
 }
