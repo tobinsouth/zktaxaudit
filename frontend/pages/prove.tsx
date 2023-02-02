50af646430a3e99acb5bba7b047a1e7c23d6dc85
Tobin South <tobin.south@gmail.com>
2023-02-02 12:49:12 -0500

    Non-function but mostly there

--- frontend/pages/prove.tsx
+++ frontend/pages/prove.tsx
@@ -21,6 +21,7 @@ import {
     ProofArtifacts,
     REQUIRED_FIELDS,
     toAscii,
+    padJSONString
 } from "../utilities/json";
 import styled from "styled-components";
 import axios from "axios";
@@ -30,6 +31,8 @@ import {
     extractPartsFromSignature,
     extractSignatureInputs,
     strHashToBuffer,
+    buffer2bits,
+    calculatePedersen
 } from "../utilities/crypto";
 import { Card } from "../components/card";
 import Link from "next/link";
@@ -53,11 +56,13 @@ export default function RedactAndProve() {
     const [isLoading, setIsLoading] = useState<number | undefined>(2);
     const [hasKeypair, setHasKeypair] = useState<boolean>(false);
     const [proofArtifacts, setProofArtifacts] = useState<ProofArtifacts | undefined>(undefined);
-    const [formattedJSON, setFormattedJSON] = useState<string | undefined>(undefined);
+    const [formattedJSON, setFormattedJSON] = useState<string>();
     const [JsonDataStore, setJsonDataStore] = useState<JSON_STORE>({});
     const [confetti, setConfetti] = useState<any>(undefined);
+    const [signatureStuff, setSignatureStuff] = useState<EddsaSignature>();
 
-    const circuitInputs = useRef<ExtractedJSONSignature & { hash: string }>();
+
+    // const circuitInputs = useRef<ExtractedJSONSignature & { hash: string }>();
 
     const setRecursiveKeyInDataStore = (keys: string[]) => {
         // TODO: This doesn't need to be recursive
@@ -77,71 +82,95 @@ export default function RedactAndProve() {
     };
 
     const generateProof = async () => {
-        try {
-            if (!isJSON(jsonText) || !`circuitInputs`.current) {
+
+        console.log("Beginning generateProof")
+        try{
+            if (!isJSON(jsonText) || !isJSON(formattedJSON)) {
+                // Checking everything is valid
                 toast.error("Invalid JSON");
                 return;
             }
-            setIsLoading(1);
-            checkJsonSchema(JsonDataStore);
-            // hardCoded.jsonProgram.map(BigInt);
 
-            // This get the S, R8, hash, and pubKey values for EdDSA verification
-            const sigParts = extractPartsFromSignature(
-                circuitInputs.current.packedSignature,
-                strHashToBuffer(circuitInputs.current.hash),
-                circuitInputs.current.servicePubkey
-            );
-
-
-
-            var revealedFields: number[] = [];
-            for (var key of REQUIRED_FIELDS) {
-                var node = getRecursiveKeyInDataStore(key, JsonDataStore);
-                if (node !== null && !isJSONStore(node)) {
-                    revealedFields.push(node["ticked"] ? 1 : 0);
-                }
-            }
-            const hash = circuitInputs.current.hash;
-            const formattedJSON = circuitInputs.current.formattedJSON;
-            const obj = preprocessJson(circuitInputs.current.jsonText, MAX_JSON_LENGTH, revealedFields);
-
-            const finalInput = {
-                ...sigParts,
-                hashJsonProgram: hash,
-                jsonProgram: formattedJSON,
-                ...obj,
-                inputReveal: revealedFields,
-            };
-
-            console.log("THE FINAL COUNTDOWN (Input) (before passing to async worker)", finalInput);
-
-            const worker = new Worker("./worker.js");
-            worker.postMessage([finalInput, "./jsonFull_final.zkey"]);
-
-            worker.onmessage = async function (e) {
-                const { proof, publicSignals, error } = e.data;
-                if (error) {
-                    toast.error("Could not generate proof, invalid signature");
-                } else {
-                    setProofArtifacts({ proof, publicSignals });
-                    console.log("PROOF SUCCESSFULLY GENERATED: ", proof, publicSignals);
-                    toast.success("Generated proof!");
-                }
-                setIsLoading(undefined);
-            };
-        } catch (ex) {
-            setIsLoading(undefined);
-            if (ex instanceof Error && ex.message.startsWith("Unable to generate proof! Missing")) {
-                toast.error(ex.message);
+            if (!signatureStuff) {
+                toast.error("Invalid signature");
                 return;
             }
-            console.error(ex);
-            toast.error("Something went wrong :(");
+
+            setIsLoading(1);
+            checkJsonSchema(JsonDataStore); // This is where we check so see if the JSON with redacting instructions is valid
+
+            const paddedJSON = padJSONString(formattedJSON, MAX_JSON_LENGTH)
+
+
+            // Find the redacted fields, and create the redaction mask array;
+            const maskArray = new Array(paddedJSON?.length).fill(0);
+            for (var key of REQUIRED_FIELDS) {
+                if (JsonDataStore[key]["ticked"]) {
+                    let fakeJson = {};
+                    fakeJson[key] = JsonDataStore[key]["value"];
+                    let fakeJsonStr = JSON.stringify(fakeJson).slice(1,-1);
+                    let redactStartIndex = paddedJSON?.indexOf(fakeJsonStr);
+                    // In redactStartIndex in undefined throw error
+                    if(!(redactStartIndex >0)){
+                        console.log("Redaction error", fakeJsonStr, paddedJSON);
+                    } else {
+                        let redactEndIndex = redactStartIndex + fakeJsonStr?.length - 2;
+                        for (var i = redactStartIndex; i < redactEndIndex; i++) {
+                            maskArray[i] = 1;
+                        }
+                        console.log("Redacting", redactStartIndex, redactEndIndex)
+                    }
+                }
+            }
+            console.log("Mask Array", maskArray);
+
+            const hashJsonProgram = await calculatePedersen(toAscii(formattedJSON));
+            console.log("Hash of JSON", hashJsonProgram);
+            
+            // Send everthing to circom
+            const finalInput = {
+                    jsonProgram: formattedJSON,
+                    redactMap: maskArray,
+                    bufferCutoff: formattedJSON?.length,
+                    pubServiceKey: signatureStuff.servicePubkey,
+                    S: signatureStuff.S,
+                    R8: signatureStuff.R8,
+                    hashJsonProgram: hashJsonProgram,
+                };
+            console.log("Final Input to prover", finalInput);
+
+            // const worker = new Worker("./worker.js");
+            // worker.postMessage([finalInput, "./jsonFull_final.zkey"]);
+
+            // worker.onmessage = async function (e) {
+            //     const { proof, publicSignals, error } = e.data;
+            //     if (error) {
+            //         toast.error("Could not generate proof, invalid signature");
+            //         console.error("Proof error", error, error.message);
+            //     } else {
+            //         setProofArtifacts({ proof, publicSignals });
+            //         console.log("PROOF SUCCESSFULLY GENERATED: ", proof, publicSignals);
+            //         toast.success("Generated proof!");
+            //     }
+            //     setIsLoading(undefined);
+            // };
+            
+        } catch (e) {
+            toast.error("Error generating proof");
+            console.error(e);
         }
+
+        // TODO: This just overrides the proof and produces fake stuff
+        setIsLoading(1);
+        await new Promise(r => setTimeout(r, 2000));
+        const publicSignals = JSON.parse(`{"name":"Tobin","height":"6"}`)
+        const proof = JSON.parse(`{"pi_a":["18294190410516669312947734766168476834733778994004982243123202741391800389625","14634973487720588390542979589285014648526082368841806376872418331570542514454","1"],"pi_b":[["15725569425907313746582249233602641223623240475518950192334299785749380959785","3875975205535093540792760784480902608387614075717885689924469452182683061235"],["6233140625442003470491093169225427542223706586435163311162231687678429973867","7659535409373758863778921753613673563919334217374674318580259056294578860993"],["1","0"]],"pi_c":["8068633704987522987686630547402334866735747304157825731702093502368451040856","10203858953998258933534809613862773075746620743995854119058227060736081190827","1"],"protocol":"groth16","curve":"bn128"}`)
+        setProofArtifacts({ proof, publicSignals });
+        setIsLoading(undefined);
     };
 
     const generateJSON = async () => {
+        console.log("Generating JSON", jsonText)
         // extractSignatureInputs takes the JSON text and extracts the signature, pubkey, and formatted JSON
         const extracted = extractSignatureInputs(jsonText); 
 
@@ -151,18 +180,18 @@ export default function RedactAndProve() {
         createJson(parsedJson, newJsonDataStore);
         setJsonDataStore(newJsonDataStore);
 
-        // We then hash the formatted JSON to send to the circuit
-        let hash = await calculatePoseidon(toAscii(extracted.formattedJSON));
-        circuitInputs.current = { ...extracted, hash };
+        // We save this for generateProof
+        setFormattedJSON(extracted.formattedJSON);
+        const signatureParts = extractPartsFromSignature(extracted.packedSignature, extracted.servicePubkey);
+        setSignatureStuff(signatureParts);
 
-        // console.log(circuitInputs.current)
+        console.log("Generated JSON")
     };
 
     useEffect(() => {
         setConfetti(new JSConfetti());
     }, []);
 
-    console.log("loading: ", isLoading);
     return (
         <>
             <Head>
@@ -192,12 +221,6 @@ export default function RedactAndProve() {
                         }}
                     />
                     <div className="py-4"></div>
-                    {formattedJSON ? (
-                        <>
-                            <div className="py-2"></div>
-                            <JsonViewer value={formattedJSON} />
-                        </>
-                    ) : null}
 
                     <Button backgroundColor="black" color="white" onClickHandler={generateJSON}>
                         Parse JSON
@@ -237,7 +260,7 @@ export default function RedactAndProve() {
                                 <a
                                     className="viewProof text-underline"
                                     target="_blank"
-                                    href={"data:text/json;charset=utf-8," + JSON.stringify(producePP(proofArtifacts.publicSignals))}
+                                    href={"data:text/json;charset=utf-8," + JSON.stringify(proofArtifacts.publicSignals)}
                                     download={"publicParams.json"}
                                     rel="noreferrer"
                                 >
